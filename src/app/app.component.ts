ecac341c239a1ec5830755d570eabd1fb81d3230
Antony Magee <antonymagee@users.noreply.github.com>
2017-10-11 22:59:42 -0600

    More of the spa course, added menu and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { FrameworkConfigService, FrameworkConfigSettings } from '../fw/services/framework-config.service';
+import { MenuService } from '../fw/services/menu.service';
+import { initialMenuItems } from './app.menu';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,8 @@ import { FrameworkConfigService, FrameworkConfigSettings } from '../fw/services/
 })
 export class AppComponent {
 
-  constructor (private frameworkConfigService: FrameworkConfigService) {
+  constructor (private frameworkConfigService: FrameworkConfigService,
+               private menuService: MenuService) {
 
     let config:FrameworkConfigSettings = {
       socialIcons: [
@@ -24,6 +27,8 @@ export class AppComponent {
     };
 
     frameworkConfigService.configure(config);
+
+    menuService.items = initialMenuItems;
 
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { FwModule } from '../fw/fw.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CountriesComponent } from './countries/countries.component';
+import { SettingsComponent } from './settings/settings.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CountriesComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
-    FwModule
+    FwModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
